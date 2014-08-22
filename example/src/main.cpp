ef1a0dc424483bdbd104f3a4ba3304d88f373a73
Philipp Rockel <philipp@rockolo.com>
2014-08-22 15:43:32 -0400

    Glut samples

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,19 +1,21 @@
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppGLFWWindow.h"
+#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
 
 	//ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
-    ofAppGLFWWindow win;
-    win.setNumSamples( 0 );
+    ofAppGlutWindow win;
+    win.setGlutDisplayString("rgba alpha double stencil samples>=4");
+    //win.setNumSamples( 0 );
     //win.setDoubleBuffering(false);
     //win.setOrientation(OF_ORIENTATION_90_LEFT);
     //win.setMultiDisplayFullscreen(true);
     //win.set
     
     ofSetupOpenGL( &win, 450, 450, OF_WINDOW );// <-------- setup the GL context
+    //ofSetupOpenGL(450,450, OF_WINDOW);
     
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
